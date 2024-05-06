6f539b4f13e8f09405882827cf8ce34968f1d1e5
Isaac <s10257760@connect.np.edu.sg>
2024-05-06 10:45:33 +0800

    Added the login page in the login view

---
+++ app/src/main/java/sg/edu/np/mad/myapplication/User.java
@@ -0,0 +1,11 @@
+package sg.edu.np.mad.myapplication;
+
+public class User {
+    String name;
+    int password;
+
+    public User(String name, int password){
+        this.name = name;
+        this.password = password;
+    }
+}
