87bcf5f6e4ba9d27ea7eba8d4f1f2ba79ac9fb49
LiangDingxuan <S10258272@connect.np.edu.sg>
2024-05-20 16:13:46 +0800

    rewards

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - Introduction, motivation/objective, app category
 - Task(s) and feature(s) allocation of each member in the team for stage 1
 - Planned task(s) and feature(s) allocation of each member in the team for stage 2
+    
---
+++ app/src/main/java/sg/edu/np/mad/myapplication/Rewards.java
@@ -0,0 +1,45 @@
+package sg.edu.np.mad.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Rewards extends AppCompatActivity {
+
+
+    LinearLayoutManager linearLayoutManagerItems;
+    RVAdapter rv_Items_Adapter;
+
+
+
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_rewards);
+        RecyclerView recyclerView = findViewById(R.id.recycler_items);
+
+        ArrayList<String> voucherData = new ArrayList<>();
+        voucherData.add("item 1");
+        voucherData.add("item 2");
+        voucherData.add("item 3");
+        voucherData.add("item 4");
+
+        linearLayoutManagerItems = new LinearLayoutManager(Rewards.this, LinearLayoutManager.VERTICAL, false);
+        RVAdapter rv_Items_Adapter = new RVAdapter(voucherData);
+        recyclerView.setLayoutManager(linearLayoutManagerItems);
+        recyclerView.setAdapter(rv_Items_Adapter);
+
+
+    }
+}
