e8eafd2f836badf1a6c16c14cf3014a6df56af6d
LiangDingxuan <S10258272@connect.np.edu.sg>
2024-05-25 18:02:06 +0800

    main page

--- app/src/main/java/sg/edu/np/mad/myapplication/RetrieveVouchers.java
+++ app/src/main/java/sg/edu/np/mad/myapplication/RetrieveVouchers.java
@@ -2,38 +2,47 @@ package sg.edu.np.mad.myapplication;
 
 import android.util.Log;
 
-import com.google.firebase.firestore.DocumentReference;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Map;
 
 public class RetrieveVouchers {
-    public static void retriveAllMenu(FirebaseFirestore db, ArrayList<Item> md){
-        DocumentReference dr = db.collection("Stores").document("Prata-Boy");
-        dr.get().addOnSuccessListener(documentSnapshot -> {
-            if(documentSnapshot.exists()){
-                // Retrive the field in the document
-                ArrayList<Map<String,String>> Menuitems = (ArrayList<Map<String,String>>) documentSnapshot.get("menuItems");
 
-                // convert all the hashmaps to classes of itemclass
-                for(Map<String,String> item: Menuitems){
-                    String name = item.get("name");
-                    String itemDesc = item.get("itemDesc");
-                    String itemID = item.get("itemID");
-                    Double price = Double.parseDouble(item.get("price"));
-                    md.add(new Item(itemID,name,itemDesc,price));
+    public static Task<ArrayList<Voucher>> retrieveVouchers(FirebaseFirestore db) {
+        TaskCompletionSource<ArrayList<Voucher>> taskCompletionSource = new TaskCompletionSource<>();
 
+        // Get a reference to the "Voucher" collection
+        CollectionReference vouchersRef = db.collection("Voucher");
+
+        vouchersRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                ArrayList<Voucher> vouchers = new ArrayList<>();
+                Log.w("RetrieveVouchers", "Start Getting documents: ", task.getException());
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    Map<String, Object> voucherDetails = document.getData();
+                    String storeID = (String) voucherDetails.get("storeID");
+                    String voucherId = (String) voucherDetails.get("voucherID");
+                    Double discount = (Double) voucherDetails.get("discount");
+                    Date validityString = (Date) voucherDetails.get("validity");
+                    Date expireString = (Date) voucherDetails.get("expire");
+                    String description = (String) voucherDetails.get("description");
+                    vouchers.add(new Voucher(storeID, voucherId, discount, validityString, expireString, description));
+                    Log.w("RetrieveVouchers", "Getting documents: ", task.getException());
 
                 }
-
-
-
-
-                // log successful retrieval
-                Log.d("This is my map",Menuitems.toString());
+                taskCompletionSource.setResult(vouchers);
+            } else {
+                Log.w("RetrieveVouchers", "Error getting documents: ", task.getException());
+                taskCompletionSource.setResult(new ArrayList<>()); // Return empty list on error
             }
-
-
         });
-}}
+
+        return taskCompletionSource.getTask();
+    }
+}
