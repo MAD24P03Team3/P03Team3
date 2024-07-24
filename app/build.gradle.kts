241605ee54fb26ec6b3300f6729df3d9562816e6
Chan-Man-Lock-Jake <jakechan.ml@gmail.com>
2024-07-24 20:50:03 +0800

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation(libs.firebase.firestore)
+    implementation("androidx.biometric:biometric:1.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
